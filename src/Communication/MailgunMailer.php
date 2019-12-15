d9462351b8269dca0a34aecbaabe6d026cdd6216
Dominique Sarrazin <sarrazin.dominique@gmail.com>
2019-12-15 14:20:01 -0500

    Update Guzzle and migrate ApiClient to use the new API

--- src/Communication/MailgunMailer.php
+++ src/Communication/MailgunMailer.php
@@ -5,34 +5,22 @@ namespace Domynation\Communication;
 use Mailgun\Mailgun;
 
 /**
- * Class MailgunMailer
- *
  * @package Domynation\Communication
  * @author Dominique Sarrazin <domynation@gmail.com>
  */
 final class MailgunMailer implements MailerInterface
 {
 
-    /**
-     * @var \Mailgun\Mailgun
-     */
-    private $mailgun;
+    private $mailer;
 
     /**
-     * @var string
-     */
-    private $domain;
-
-    /**
-     * MailgunMailer constructor.
-     *
      * @param string $apiKey
      * @param string $domain
      */
     public function __construct(string $apiKey, string $domain)
     {
-        $this->mailgun = new Mailgun($apiKey);
-        $this->domain = $domain;
+        $transport = new \Symfony\Component\Mailer\Bridge\Mailgun\Transport\MailgunApiTransport($apiKey, $domain);
+        $this->mailer = new \Symfony\Component\Mailer\Mailer($transport);
     }
 
     /**
@@ -40,21 +28,16 @@ final class MailgunMailer implements MailerInterface
      */
     public function send(EmailMessage $message, $data = [])
     {
-        $messageData = [
-            'to'      => $message->recipients,
-            'from'    => $message->getFullSender(),
-            'subject' => $message->subject,
-            'text'    => $message->body
-        ];
+        $email = (new \Symfony\Component\Mime\Email())
+            ->from($message->getFullSender())
+            ->to(...$message->recipients)
+            ->subject($message->subject)
+            ->text($message->body);
 
         if (!empty($message->bcc)) {
-            $messageData['bcc'] = $message->bcc;
+            $email->bcc($message->bcc);
         }
 
-        // Set the mailgun domain
-        $domain = !empty($data['domain']) ? $data['domain'] : $this->domain;
-
-        // Send the message
-        $this->mailgun->sendMessage($domain, $messageData);
+        $this->mailer->send($email);
     }
 }
